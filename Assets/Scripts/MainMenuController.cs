a784fb4047c9c556ec8093c9a6e5bda8461d50cb
isevo353 <vanasevostanov33@gmail.com>
2026-03-26 20:06:50 +0300

    компания

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,12 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // ����� ��� ������ "������"
+    // ����� ��� ������ "������" / "����������� �����"
     public void PlayGame()
     {
         Debug.Log("�������� ������� �����");
         SceneManager.LoadScene("GameScene"); // ��� ����� ������� �����
     }
+
+    // ����� ��� ������ "������"
+    public void OpenStoryMenu()
+    {
+        Debug.Log("Открытие меню сюжета");
+        SceneManager.LoadScene("StoryMenuScene");
+    }
+
+    // Пока что все уровни ведут на одну и ту же сцену
+    public void LoadLevel1() => SceneManager.LoadScene("GameScene");
+    public void LoadLevel2() => SceneManager.LoadScene("GameScene");
+    public void LoadLevel3() => SceneManager.LoadScene("GameScene");
 
     // ����� ��� ������ "���������"
     public void OpenSettings()
